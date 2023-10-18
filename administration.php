2609c159af51174b0923fce34fcab3f3d2a0d9a3
AxelBaranowski3il <117455133+AxelBaranowski3il@users.noreply.github.com>
2023-10-18 14:55:47 +0200

    afficher les images de la base

--- administration.php
+++ administration.php
@@ -8,65 +8,96 @@
 
 </head>
 <body>
-<div class="container_haut_de_page">
-  <h1>Page d'administration</h1>
-  <button class="btn_retour_acc"><a href="index.php">Retour accueil</a></button>
-  <button class="btn_retour_acc"><a href="auth.php">Deconnexion</a></button>
-</div>
+  <div class="container_haut_de_page">
+    <h1>Page d'administration</h1>
+    <button class="btn_retour_acc"><a href="index.php">Retour accueil</a></button>
+    <button class="btn_retour_acc"><a href="auth.php">Deconnexion</a></button>
+  </div>
 
-<div class="container">
+  <div class="container">
 
-  <h2>Ajouter un utilisateur</h2>
-  <form method="post" action="administration.php">
-    <label>Login : <input type="text" name="login" required></label>
-    <label>Mot de passe : <input type="password" name="password" required></label>
-    <button type="submit" name="ajouter">Ajouter</button>
-  </form>
+    <h2>Ajouter un utilisateur</h2>
+    <form method="post" action="administration.php">
+      <label>Login : <input type="text" name="login" required></label>
+      <label>Mot de passe : <input type="password" name="password" required></label>
+      <button type="submit" name="ajouter">Ajouter</button>
+    </form>
 
-  <h2>Liste des utilisateurs</h2>
-  <table>
-    <tr>
-      <th>Login</th>
-      <th>Mot de passe</th>
-      <th>Action</th>
-    </tr>
-    <?php
-    $serveur = "localhost";
-    $utilisateur = "root";
-    $motdepasse = "";
-    $basededonnees = "projet_gite";
+    <h2>Liste des utilisateurs</h2>
+    <table>
+      <tr>
+        <th>Login</th>
+        <th>Mot de passe</th>
+        <th>Action</th>
+      </tr>
+      <?php
+      $serveur = "localhost";
+      $utilisateur = "root";
+      $motdepasse = "";
+      $basededonnees = "projet_gite";
 
-    $connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
+      $connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
 
-    if ($connexion->connect_error) {
-      die("La connexion à la base de données a échoué : " . $connexion->connect_error);
-    }
+      if ($connexion->connect_error) {
+        die("La connexion à la base de données a échoué : " . $connexion->connect_error);
+      }
 
-    if (isset($_POST['ajouter'])) {
-      $login = $_POST['login'];
-      $password = $_POST['password'];
-      $requete = $connexion->prepare("INSERT INTO user (login, passwd) VALUES (?, ?)");
-      $requete->bind_param("ss", $login, $password);
-      $requete->execute();
-    }
+      if (isset($_POST['ajouter'])) {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        $requete = $connexion->prepare("INSERT INTO user (login, passwd) VALUES (?, ?)");
+        $requete->bind_param("ss", $login, $password);
+        $requete->execute();
+      }
 
-    if (isset($_GET['supprimer'])) {
-      $login = $_GET['supprimer']; // Récupérer le login à partir de la requête GET
-      $requete = $connexion->prepare("DELETE FROM user WHERE login = ?");
-      $requete->bind_param("s", $login);
-      $requete->execute();
-    }
+      if (isset($_GET['supprimer'])) {
+        $login = $_GET['supprimer']; // Récupérer le login à partir de la requête GET
+        $requete = $connexion->prepare("DELETE FROM user WHERE login = ?");
+        $requete->bind_param("s", $login);
+        $requete->execute();
+      }
 
-    $requete = $connexion->query("SELECT * FROM user");
-    while ($utilisateur = $requete->fetch_assoc()) {
-      echo "<tr>";
-      echo "<td>{$utilisateur['login']}</td>";
-      echo "<td>{$utilisateur['passwd']}</td>";
-      echo "<td><a href='administration.php?supprimer={$utilisateur['login']}'>Supprimer</a></td>";
-      echo "</tr>";
-    }
-    ?>
-  </table>
+      $requete = $connexion->query("SELECT * FROM user");
+      while ($utilisateur = $requete->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>{$utilisateur['login']}</td>";
+        echo "<td>{$utilisateur['passwd']}</td>";
+        echo "<td><a href='administration.php?supprimer={$utilisateur['login']}'>Supprimer</a></td>";
+        echo "</tr>";
+      }
+      ?>
+    </table>
+
+    <h2>Liste des images</h2>
+    <table>
+      <tr>
+        <th>Image</th>
+        <th>Description</th>
+        <th>Date de création</th>
+      </tr>
+      <?php
+      $serveur = "localhost";
+      $utilisateur = "root";
+      $motdepasse = "";
+      $basededonnees = "projet_gite";
+
+      $connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
+
+      if ($connexion->connect_error) {
+        die("La connexion à la base de données a échoué : " . $connexion->connect_error);
+      }
+      $resultatImages = $connexion->query("SELECT * FROM images");
+
+      while ($image = $resultatImages->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td><img src='" . $image['nom_fichier'] . "' alt='Image'></td>";
+        echo "<td>" . $image['description'] . "</td>";
+        echo "<td>" . $image['date_creation'] . "</td>";
+        echo "</tr>";
+      }
+      ?>
+    </table>
+
   </body>
 </div>
 </html>
